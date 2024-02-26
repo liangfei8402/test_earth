dc688ba13ff875c02ec85a4f3fbdb6891d341630
liangfei8402 <liangfei8402@163.com>
2024-02-26 10:55:43 +0800

    第一次提交cesium

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import {onMounted} from "vue";
+  import * as Cesium from 'cesium';
+  onMounted(()=>{
+    let viewer = new Cesium.Viewer("cesiumContainer",)
+})
+</script>
+
+<template>
+  <div id="cesiumContainer">
+  </div>
+</template>
+<style >
+  #cesiumContainer{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+</style>
